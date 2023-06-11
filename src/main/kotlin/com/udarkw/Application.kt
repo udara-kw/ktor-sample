6d38567a0a405078fc77e94d2ebc086d6ba5c1e3
udarkw <udarkw@gmail.com>
2023-06-11 23:24:27 +0530

    Add CORS support

--- src/main/kotlin/com/udarkw/Application.kt
+++ src/main/kotlin/com/udarkw/Application.kt
@@ -4,17 +4,25 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.udarkw.plugins.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 9665, host = "127.0.0.1", module = Application::module)
             .start(wait = true)
 }
 
+
+
 fun Application.module() {
+    install(CORS) {
+        anyHost()
+    }
+
 //    Uncomment Security, HTTP and monitoring if needed
 //    configureSecurity()
 //    configureHTTP()
 //    configureMonitoring()
+
     configureSerialization()
     configureRouting()
 }
