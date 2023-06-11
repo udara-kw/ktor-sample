766a8f7e0bf647f408f65713151c4b0c565e4a59
udarkw <udarkw@gmail.com>
2023-06-11 23:25:33 +0530

    Code cleanup

--- src/main/kotlin/com/udarkw/Application.kt
+++ src/main/kotlin/com/udarkw/Application.kt
@@ -1,17 +1,16 @@
 package com.udarkw
 
+import com.udarkw.plugins.configureRouting
+import com.udarkw.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.udarkw.plugins.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 9665, host = "127.0.0.1", module = Application::module)
-            .start(wait = true)
+        .start(wait = true)
 }
-
-
 
 fun Application.module() {
     install(CORS) {
--- src/main/kotlin/com/udarkw/routes/OrderRoutes.kt
+++ src/main/kotlin/com/udarkw/routes/OrderRoutes.kt
@@ -3,7 +3,6 @@ package com.udarkw.routes
 import com.udarkw.models.orderStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
